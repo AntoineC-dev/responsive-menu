aa38cbd2cddd7faad3ffb1339ed1d106ae6cd0f0
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-27 20:33:10 -0400

    Add scripts folder to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/main.ts"],
+  content: ["./index.html", "./src/main.ts", "./src/**/*.ts"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {},
   },
   plugins: [],
