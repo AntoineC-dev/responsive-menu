24a0e75e02b3907edbbc3a68996d5dc316c820af
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-27 23:26:55 -0400

    Update mobile config w/ animation data

--- src/config/mobile-menu.config.ts
+++ src/config/mobile-menu.config.ts
@@ -1,8 +1,25 @@
+import { primaryNav } from "./selectors.config";
+
+function getPrimaryNavAnimationDelay() {
+  let delay = 150; // tailwind default
+  primaryNav.classList.forEach((item) => {
+    if (item.includes("duration-")) {
+      delay = parseInt(item.substring(9));
+    }
+  });
+  return delay;
+}
+
 export interface MobileMenuConfig {
   hamburgerTranslate: number; // = verticalGap / lineHeight
   isOpen: boolean;
+  primaryNavAnimationDuration: number;
+  primaryNavItemsDelay: number;
 }
+
 export const mobileMenuConfig: MobileMenuConfig = {
   hamburgerTranslate: 6 + 4,
   isOpen: false,
+  primaryNavAnimationDuration: getPrimaryNavAnimationDelay(),
+  primaryNavItemsDelay: 25,
 };
